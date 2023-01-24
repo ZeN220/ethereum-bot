258037402752f6fbd66ffae8d194da0019adebec
ZeN220 <artaganmalsagov07@gmail.com>
2023-01-24 22:24:34 +0300

    added "commit" method

--- src/database/repositories/base.py
+++ src/database/repositories/base.py
@@ -4,3 +4,6 @@ from sqlalchemy.ext.asyncio import AsyncSession
 class BaseRepository:
     def __init__(self, session: AsyncSession):
         self.session = session
+
+    async def commit(self):
+        await self.session.commit()
