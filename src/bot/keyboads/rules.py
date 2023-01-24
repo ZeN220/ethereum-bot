2abbd0894d72326a2d04d42c8890aaea721d2016
ZeN220 <artaganmalsagov07@gmail.com>
2023-01-24 22:39:27 +0300

    change callback data rules keyboard

--- src/bot/keyboads/rules.py
+++ src/bot/keyboads/rules.py
@@ -5,6 +5,6 @@ from aiogram.utils.keyboard import InlineKeyboardBuilder
 def get_rules_keyboard() -> InlineKeyboardMarkup:
     builder = InlineKeyboardBuilder()
     builder.row(
-        InlineKeyboardButton(text="Согласен ✅", callback_data="accept"),
+        InlineKeyboardButton(text="Согласен ✅", callback_data="accept_rules"),
     )
     return builder.as_markup()
